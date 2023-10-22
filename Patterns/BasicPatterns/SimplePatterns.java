4a242a8b5b25e6c0fa478dea176aaa42bd6823a6
Teja <tejendrateja127@gmail.com>
2023-10-22 14:43:38 +0530

    patterns problems

--- Patterns/BasicPatterns/SimplePatterns.java
+++ Patterns/BasicPatterns/SimplePatterns.java
@@ -4,13 +4,20 @@ public class SimplePatterns {
         p1(5);
         System.out.println("\nSquare pattern rows == cols\n");
         p2(10);
+        System.out.println("\nRight angle triangle with 1-n numbers\n");
+        p3(5);
+        System.out.println("\nArrow pattern using stars\n");
+        p4(5);
+        System.out.println("\nReverse right angle triangle pattern\n");
+        p5(5);
+
     }
 
     static void p1(int n){
         // right angle triangle pattern
         for(int rows = 1; rows<n; rows++){
             for(int cols = 1; cols<=rows; cols++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println("");
         }
@@ -20,9 +27,47 @@ public class SimplePatterns {
         // square pattern equal no of rows and cols
         for(int row = 1; row <n; row++){
             for(int col = 1; col<= n; col++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+
+    static void p3(int n){
+        // right angle triangle with 1-col numbers 
+        for(int row = 1; row<n; row++){
+            for(int col = 1; col<=row; col++){
+                System.out.print(col+" ");
+            }
+            System.out.println("");
+        }
+    }
+
+    static void p4(int n){
+        // arrow pattern here no of rows = 2n-1
+        for(int row = 1; row<=2*n; row++){
+            if(row<n){
+                for(int col = 1; col<= row; col++){
+                    System.out.print("*");
+                }   
+            }else{
+                for(int col = 1; col<=2*n-row+1; col++){
+                    System.out.print("*");
+                    
+                }
+            }
+            System.out.println("");
+            
+        }
+    }
+
+    static void p5(int n){
+        for(int row = 1; row<=n; row++){
+            for(int col = 1; col<= n-row+1; col++){
                 System.out.print("*");
             }
             System.out.println("");
+            
         }
     }
 }

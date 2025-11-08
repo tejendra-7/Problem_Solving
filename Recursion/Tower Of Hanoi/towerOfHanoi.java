14756df0a7ac9502546e709f9f7300f6a462ded5
Teja <tejendrateja127@gmail.com>
2025-11-08 11:02:57 +0530

    tower of Hanoi

---
+++ Recursion/Tower Of Hanoi/towerOfHanoi.java
@@ -0,0 +1,19 @@
+public class towerOfHanoi {
+    public static void main(String[] args) {
+        int n = 3;
+        hanoi(n, "S", "H", "D");
+    }
+
+    public static void hanoi(int n , String src , String helper, String dest){
+        if(n == 1){
+            System.out.println("transfer Disk "+ n + " from "+ src + " to " + dest);
+            return;
+        }
+
+        hanoi(n-1, src ,dest ,helper );
+
+        System.out.println("transfer disk "+ n + " from "+ src + " to "+ dest);
+        hanoi(n-1, helper, src, dest);
+    }
+}
+

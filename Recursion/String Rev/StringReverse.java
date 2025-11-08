04290302af865691b84612c29ccf37543c614ccc
Teja <tejendrateja127@gmail.com>
2025-11-08 11:16:32 +0530

    String rev

---
+++ Recursion/String Rev/StringReverse.java
@@ -0,0 +1,16 @@
+public class StringReverse {
+    public static void strRev(String str , int idx){
+        if(idx == 0){
+            System.out.print(str.charAt(idx));
+            return;
+        }
+
+        System.out.print(str.charAt(idx));
+        strRev(str, idx-1);
+    }
+
+    public static void main(String[] args) {
+        String str = "abcd";
+        strRev(str, str.length()-1);
+    }
+}
